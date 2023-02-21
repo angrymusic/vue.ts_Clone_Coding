3859b5b4f50a3145d2a291f5a01d5db3f4cce514
angrymusic <angrymusic@naver.com>
2023-02-21 15:34:41 +0900

    인스타 검색바 토글 focusout구현

--- src/views/InstagramView.vue
+++ src/views/InstagramView.vue
@@ -94,11 +94,21 @@
                 stroke-linejoin="round"
                 stroke-width="2"
               ></path>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="16.511"
+                x2="22"
+                y1="16.511"
+                y2="22"
+              ></line>
             </svg>
           </div>
         </div>
-        <input @focus="clickSearch" class="input-search col-10" placeholder="검색" />
+        <input @focusin="clickSearch" @focusout="clickSearch" class="input-search col-10" placeholder="검색" />
         <div v-show="searchBar" class="after-click col row items-center justify-between">
           <div class="exit-search col row justify-end" @click="clickSearch">⨉</div>
         </div>
@@ -130,7 +140,9 @@
             <div class="count-follow more-info">팔로우 <strong>209</strong></div>
           </div>
           <div class="desktop bottom-desc col">
-            <strong>Son HeungMin(손흥민)🇰🇷</strong><br />@spursofficial ⚽️🇰🇷🇬🇧 @adidasfootball <br /><a class="powerlink" href="https://www.facebook.com/HeungMinSonOfficial"
+            <strong>Son HeungMin(손흥민)🇰🇷</strong><br />@spursofficial ⚽️🇰🇷🇬🇧 @adidasfootball <br /><a
+              class="powerlink"
+              href="https://www.facebook.com/HeungMinSonOfficial"
               >www.facebook.com/HeungMinSonOfficial</a
             >
           </div>
@@ -153,7 +165,9 @@
         </div>
 
         <div class="mobile q-ml-md q-mt-md q-mb-md bottom-desc">
-          <strong>Son HeungMin(손흥민)🇰🇷</strong><br />@spursofficial ⚽️🇰🇷🇬🇧 @adidasfootball <br /><a class="powerlink" href="https://www.facebook.com/HeungMinSonOfficial"
+          <strong>Son HeungMin(손흥민)🇰🇷</strong><br />@spursofficial ⚽️🇰🇷🇬🇧 @adidasfootball <br /><a
+            class="powerlink"
+            href="https://www.facebook.com/HeungMinSonOfficial"
             >www.facebook.com/HeungMinSonOfficial</a
           >
         </div>
@@ -173,18 +187,105 @@
         </div>
         <q-tabs switch-indicator v-model="tab" class="separator tabs color-fafafa text-black">
           <q-tab class="col-1" name="posts" label="">
-            <svg aria-label="게시물" :class="{ selected: tab == 'posts' ? true : false }" color="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
-              <rect fill="none" height="18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="3"></rect>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="9.015" x2="9.015" y1="3" y2="21"></line>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14.985" x2="14.985" y1="3" y2="21"></line>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="9.015" y2="9.015"></line>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="14.985" y2="14.985"></line></svg
+            <svg
+              aria-label="게시물"
+              :class="{ selected: tab == 'posts' ? true : false }"
+              color="#8e8e8e"
+              height="24"
+              role="img"
+              viewBox="0 0 24 24"
+              width="24"
+            >
+              <rect
+                fill="none"
+                height="18"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                width="18"
+                x="3"
+                y="3"
+              ></rect>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="9.015"
+                x2="9.015"
+                y1="3"
+                y2="21"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="14.985"
+                x2="14.985"
+                y1="3"
+                y2="21"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="21"
+                x2="3"
+                y1="9.015"
+                y2="9.015"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="21"
+                x2="3"
+                y1="14.985"
+                y2="14.985"
+              ></line></svg
           ></q-tab>
           <q-tab class="col-1" name="reels" label=""
-            ><svg aria-label="릴스" :class="{ selected: tab == 'reels' ? true : false }" color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
+            ><svg
+              aria-label="릴스"
+              :class="{ selected: tab == 'reels' ? true : false }"
+              color="#8e8e8e"
+              fill="#8e8e8e"
+              height="24"
+              role="img"
+              viewBox="0 0 24 24"
+              width="24"
+            >
               <line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="2.049" x2="21.95" y1="7.002" y2="7.002"></line>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="13.504" x2="16.362" y1="2.001" y2="7.002"></line>
-              <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="7.207" x2="10.002" y1="2.11" y2="7.002"></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="13.504"
+                x2="16.362"
+                y1="2.001"
+                y2="7.002"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="7.207"
+                x2="10.002"
+                y1="2.11"
+                y2="7.002"
+              ></line>
               <path
                 d="M2 12.001v3.449c0 2.849.698 4.006 1.606 4.945.94.908 2.098 1.607 4.946 1.607h6.896c2.848 0 4.006-.699 4.946-1.607.908-.939 1.606-2.096 1.606-4.945V8.552c0-2.848-.698-4.006-1.606-4.945C19.454 2.699 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.546 2 5.704 2 8.552Z"
                 fill="none"
@@ -200,7 +301,15 @@
           ></q-tab>
 
           <q-tab class="col-1" name="write" label="">
-            <svg aria-label="글작성" :class="{ selected: tab == 'write' ? true : false }" color="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
+            <svg
+              aria-label="글작성"
+              :class="{ selected: tab == 'write' ? true : false }"
+              color="#8e8e8e"
+              height="24"
+              role="img"
+              viewBox="0 0 24 24"
+              width="24"
+            >
               <path
                 d="M10.201 3.797 12 1.997l1.799 1.8a1.59 1.59 0 0 0 1.124.465h5.259A1.818 1.818 0 0 1 22 6.08v14.104a1.818 1.818 0 0 1-1.818 1.818H3.818A1.818 1.818 0 0 1 2 20.184V6.08a1.818 1.818 0 0 1 1.818-1.818h5.26a1.59 1.59 0 0 0 1.123-.465Z"
                 fill="none"
@@ -217,7 +326,16 @@
                 stroke-linejoin="round"
                 stroke-width="2"
               ></path>
-              <circle cx="12.072" cy="11.075" fill="none" r="3.556" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle></svg
+              <circle
+                cx="12.072"
+                cy="11.075"
+                fill="none"
+                r="3.556"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></circle></svg
           ></q-tab>
         </q-tabs>
       </div>
@@ -232,7 +350,15 @@
 
         <q-tab-panels v-model="tab" animated class="color-fafafa text-center">
           <q-tab-panel name="posts" class="row q-col-gutter-md">
-            <Post v-for="(item, index) in imgList" :key="index" :title="item.title" :context="item.context" :img="item.img" @deletePost="deletePost" class="col-4"></Post>
+            <Post
+              v-for="(item, index) in imgList"
+              :key="index"
+              :title="item.title"
+              :context="item.context"
+              :img="item.img"
+              @deletePost="deletePost"
+              class="col-4"
+            ></Post>
           </q-tab-panel>
 
           <q-tab-panel name="reels" class="">
