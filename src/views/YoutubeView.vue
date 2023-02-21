7eb3e6a762c42e786cb17d1a54ac6aa17ac98a27
angrymusic <angrymusic@naver.com>
2023-02-21 09:49:26 +0900

    youtube darkmode

--- src/views/YoutubeView.vue
+++ src/views/YoutubeView.vue
@@ -141,7 +141,7 @@
 <template>
   <q-layout class="no-shadow youtube">
     <q-header class="bg-white">
-      <q-toolbar v-if="searchBar" class="row" :class="{'darkMode'
+      <q-toolbar v-if="searchBar" class="row" :class="{'dark-mode'
       :$q.dark.isActive}">
         <div class="voice-button button flex flex-center" @click="closeSearchBar">
           <div class="icon">
@@ -149,7 +149,7 @@
               viewBox="0 0 24 24"
               preserveAspectRatio="xMidYMid meet"
               focusable="false"
-              class=""
+              :class="{'dark-svg':$q.dark.isActive}"
               style=""
             >
               <g mirror-in-rtl="" class="style-scope yt-icon">
@@ -161,7 +161,7 @@
 
         <div class="col row">
           <div class="search-bar col row items-center" :class="{ 'border-blue': clicked, 'border-gray': !clicked }">
-            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="button2 translateY" v-if="clicked" style="">
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="button2 translateY" :class="{'dark-svg':$q.dark.isActive}" v-if="clicked" style="">
               <g>
                 <path
                   d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"
@@ -169,11 +169,11 @@
                 ></path>
               </g>
             </svg>
-            <input @focusin="clickSearch(true)" @focusout="clickSearch(false)" class="search-box" placeholder="검색" />
+            <input @focusin="clickSearch(true)" @focusout="clickSearch(false)" class="search-box col" :class="{'dark-font':$q.dark.isActive}" placeholder="검색" />
           </div>
 
-          <div class="search-button row items-center">
-            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="q-mr-md icon translateY" style="">
+          <div class="search-button row items-center" :class="{'dark-bg':$q.dark.isActive}">
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="q-mr-md icon translateY" :class="{'dark-svg':$q.dark.isActive}" style="">
               <g>
                 <path
                   d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"
@@ -197,7 +197,7 @@
         </div>
       </q-toolbar>
       <q-toolbar v-if="!searchBar" class="" :class="{'dark-mode':$q.dark.isActive}">
-        <q-btn :class="{'dark-svg':$q.dark.isActive}" dense flat round icon="menu" color="black" @click="toggleLeftDrawer" />
+        <q-btn :class="{'dark-svg':$q.dark.isActive, 'dark-hover': $q.dark.isActive}" dense flat round icon="menu" color="black" @click="toggleLeftDrawer" />
 
         <q-toolbar-title class="row items-center">
           <q-img v-if="!$q.dark.isActive" src="../assets/youtubelogo.svg" width="90px" />
@@ -240,7 +240,7 @@
                   ></path>
                 </g>
               </svg>
-              <input @focusin="clickSearch(true)" @focusout="clickSearch(false)" class="search-box" placeholder="검색" />
+              <input @focusin="clickSearch(true)" @focusout="clickSearch(false)" class="search-box" :class="{'dark-font':$q.dark.isActive}" placeholder="검색" />
             </div>
 
             <div class="search-button row items-center" :class="{'dark-bg':$q.dark.isActive}">
@@ -254,7 +254,7 @@
               </svg>
             </div>
 
-            <div class="voice-button button row items-center justify-center">
+            <div class="voice-button button row items-center justify-center" :class="{'dark-hover': $q.dark.isActive}">
               <div class="icon">
                 <svg viewBox="0 0 24 24" :class="{'dark-svg':$q.dark.isActive}" preserveAspectRatio="xMidYMid meet" focusable="false" class="">
                   <g class="">
@@ -281,7 +281,7 @@
             </div>
           </div>
           <div class="login-button row items-center border-gray">
-            <svg :class="{'dark-svg':$q.dark.isActive}" viewBox="0 0 24 24" fill="rgb(6,95,212)" preserveAspectRatio="xMidYMid meet" focusable="false" class="icon" style="">
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="icon" style="fill:#1c62b9">
               <g class="style-scope yt-icon">
                 <path
                   d="M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M12,3c4.96,0,9,4.04,9,9 c0,1.42-0.34,2.76-0.93,3.96c-1.53-1.72-3.98-2.89-7.38-3.03C14.57,12.6,16,10.97,16,9c0-2.21-1.79-4-4-4C9.79,5,8,6.79,8,9 c0,1.97,1.43,3.6,3.31,3.93c-3.4,0.14-5.85,1.31-7.38,3.03C3.34,14.76,3,13.42,3,12C3,7.04,7.04,3,12,3z M9,9c0-1.65,1.35-3,3-3 s3,1.35,3,3c0,1.65-1.35,3-3,3S9,10.65,9,9z M12,21c-3.16,0-5.94-1.64-7.55-4.12C6.01,14.93,8.61,13.9,12,13.9 c3.39,0,5.99,1.03,7.55,2.98C17.94,19.36,15.16,21,12,21z"
@@ -364,7 +364,8 @@
           <div class="category" :class="{'dark-bg':$q.dark.isActive, 'selected-category':category=='music', 'dark-selected-category':category=='music'&&$q.dark.isActive}" @click="selectCategory('music')">음악</div>
           <div class="category" :class="{'dark-bg':$q.dark.isActive, 'selected-category':category=='game', 'dark-selected-category':category=='game'&&$q.dark.isActive}" @click="selectCategory('game')">게임</div>
           <div class="category" :class="{'dark-bg':$q.dark.isActive, 'selected-category':category=='news', 'dark-selected-category':category=='news'&&$q.dark.isActive}" @click="selectCategory('news')">뉴스</div>
-          <div class="col self-end"><q-toggle v-model="darkMode" label="다크 모드"/></div>
+          <div class="col"></div>
+          <div class="q-mr-md"><q-toggle v-model="darkMode" label="다크 모드"/></div>
         </div>
         <div class="row q-mt-xs" :class="{ 'q-col-gutter-xs': $q.screen.lt.md, 'q-col-gutter-md': $q.screen.gt.sm }">
           <YoutubeVideo
@@ -409,6 +410,7 @@
   color: white !important;
   fill: white !important;
 }
+
 .dark-mode{
   background-color: black !important;
 }
@@ -447,6 +449,9 @@
     background-color: white !important;
     color:black;
   }
+  .dark-font{
+    color: white;
+  }
   .login-button {
     padding: 0 10px;
     font-size: 14px;
@@ -471,6 +476,9 @@
   }
   .voice-button:hover {
     background-color: rgba(0, 0, 0, 0.1);
+  }
+  .dark-hover:hover {
+    background-color: #303030 !important;
   }
   .icon {
     width: 24px;
